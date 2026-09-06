86686dec33e0c2eb1b15ec53c75149564c2a4d8a
rushgooal-cell <rushgooal@gmail.com>
2026-09-06 23:03:50 +0300

    chore: update gradle and kotlin versions for Gradle 9.0 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("com.android.library") version "8.2.0" apply false
-    kotlin("android") version "1.9.20" apply false
-    kotlin("jvm") version "1.9.20" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("com.android.library") version "8.5.0" apply false
+    kotlin("android") version "1.9.25" apply false
+    kotlin("jvm") version "1.9.25" apply false
 }
 
 buildscript {
